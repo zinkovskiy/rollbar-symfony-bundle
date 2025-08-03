5cfc586f30e9eafbc40f754ef41979191c0cd6c0
Anton_sz <zinkovskiy.as@gmail.com>
2025-08-03 19:48:43 +0200

    Introduce scrub environment variables option

--- tests/Unit/Service/Scrubber/DefaultScrubberTest.php
+++ tests/Unit/Service/Scrubber/DefaultScrubberTest.php
@@ -2,20 +2,6 @@
 
 declare(strict_types=1);
 
-// This is the namespace of the class under test.
-// We are defining a global function here to trick PHP's namespace resolution.
-
-namespace SFErTrack\RollbarSymfonyBundle\Service\Scrubber;
-
-/** @return array<string, string> list of env variables, where key is variable name and value - it's value */
-function getenv(?string $name = null, bool $local_only = false): array
-{
-    return [
-        'APP_ENV' => 'prod',
-        'APP_SECRET' => '37d2f99b0fcffa4818228715851a68a2',
-    ];
-}
-
 namespace SFErTrack\RollbarSymfonyBundle\Tests\Unit\Service\Scrubber;
 
 use PHPUnit\Framework\TestCase;
@@ -23,9 +9,13 @@ use SFErTrack\RollbarSymfonyBundle\Service\Scrubber\DefaultScrubber;
 
 class DefaultScrubberTest extends TestCase
 {
+    use SymfonyDotenvTrait;
+
     /** @test */
     public function scrubSpecificVariable(): void
     {
+        $this->setSymfonyEnvVars();
+
         $data = json_decode(
             file_get_contents(__DIR__.'/../../data/scrubber/DefaultScrubber/http-request-with-env-vars.json'),
             true
@@ -42,11 +32,15 @@ class DefaultScrubberTest extends TestCase
 
         $actualData = $service->scrub($data, '***');
         $this->assertEquals($expectedData, $actualData);
+
+        $this->unsetSymfonyEnvVars();
     }
 
     /** @test */
     public function scrubAllEnvVariables(): void
     {
+        $this->setSymfonyEnvVars();
+
         $data = json_decode(
             file_get_contents(__DIR__.'/../../data/scrubber/DefaultScrubber/http-request-with-env-vars.json'),
             true
@@ -63,11 +57,15 @@ class DefaultScrubberTest extends TestCase
 
         $actualData = $service->scrub($data, '***');
         $this->assertEquals($expectedData, $actualData);
+
+        $this->unsetSymfonyEnvVars();
     }
 
     /** @test */
     public function scrubAllEnvVariablesExceptWhitelisted(): void
     {
+        $this->setSymfonyEnvVars();
+
         $data = json_decode(
             file_get_contents(__DIR__.'/../../data/scrubber/DefaultScrubber/http-request-with-env-vars.json'),
             true
@@ -84,5 +82,7 @@ class DefaultScrubberTest extends TestCase
 
         $actualData = $service->scrub($data, '***');
         $this->assertEquals($expectedData, $actualData);
+
+        $this->unsetSymfonyEnvVars();
     }
 }
