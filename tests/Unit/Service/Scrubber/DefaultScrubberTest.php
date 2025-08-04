3f6326307fab1db71ca1fcb1cf68374a0fd14670
Anton_sz <zinkovskiy.as@gmail.com>
2025-08-04 15:33:45 +0200

    Bugfix: scrub environment variables defined outside, without .env files

--- tests/Unit/Service/Scrubber/DefaultScrubberTest.php
+++ tests/Unit/Service/Scrubber/DefaultScrubberTest.php
@@ -4,85 +4,61 @@ declare(strict_types=1);
 
 namespace SFErTrack\RollbarSymfonyBundle\Tests\Unit\Service\Scrubber;
 
-use PHPUnit\Framework\TestCase;
 use SFErTrack\RollbarSymfonyBundle\Service\Scrubber\DefaultScrubber;
 
-class DefaultScrubberTest extends TestCase
+class DefaultScrubberTest extends ScrubEnvTestCase
 {
-    use SymfonyDotenvTrait;
-
     /** @test */
     public function scrubSpecificVariable(): void
     {
-        $this->setSymfonyEnvVars();
-
         $data = json_decode(
             file_get_contents(__DIR__.'/../../data/scrubber/DefaultScrubber/http-request-with-env-vars.json'),
-            true
-        );
-
-        $expectedData = json_decode(
-            file_get_contents(
-                __DIR__.'/../../data/scrubber/DefaultScrubber/http-request-scrubbed-specific-env-var.json'
-            ),
             true
         );
 
         $service = new DefaultScrubber(['APP_SECRET'], [], false);
 
         $actualData = $service->scrub($data, '***');
-        $this->assertEquals($expectedData, $actualData);
 
-        $this->unsetSymfonyEnvVars();
+        $actualDataJson = json_encode($actualData, JSON_THROW_ON_ERROR);
+
+        $this->assertStringNotContainsStringIgnoringCase(self::APP_SECRET, $actualDataJson);
+        $this->assertStringContainsStringIgnoringCase(str_replace('/', '\\/', self::DATABASE_DSN), $actualDataJson);
     }
 
     /** @test */
     public function scrubAllEnvVariables(): void
     {
-        $this->setSymfonyEnvVars();
-
         $data = json_decode(
             file_get_contents(__DIR__.'/../../data/scrubber/DefaultScrubber/http-request-with-env-vars.json'),
-            true
-        );
-
-        $expectedData = json_decode(
-            file_get_contents(
-                __DIR__.'/../../data/scrubber/DefaultScrubber/http-request-scrubbed-all-env-vars.json'
-            ),
             true
         );
 
         $service = new DefaultScrubber([], [], true);
 
         $actualData = $service->scrub($data, '***');
-        $this->assertEquals($expectedData, $actualData);
 
-        $this->unsetSymfonyEnvVars();
+        $actualDataJson = json_encode($actualData, JSON_THROW_ON_ERROR);
+
+        $this->assertStringNotContainsStringIgnoringCase(self::APP_SECRET, $actualDataJson);
+        $this->assertStringNotContainsStringIgnoringCase(str_replace('/', '\\/', self::DATABASE_DSN), $actualDataJson);
     }
 
     /** @test */
     public function scrubAllEnvVariablesExceptWhitelisted(): void
     {
-        $this->setSymfonyEnvVars();
-
         $data = json_decode(
             file_get_contents(__DIR__.'/../../data/scrubber/DefaultScrubber/http-request-with-env-vars.json'),
-            true
-        );
-
-        $expectedData = json_decode(
-            file_get_contents(
-                __DIR__.'/../../data/scrubber/DefaultScrubber/http-request-scrubbed-all-except-whitelisted.json'
-            ),
             true
         );
 
         $service = new DefaultScrubber([], ['APP_SECRET'], true);
 
         $actualData = $service->scrub($data, '***');
-        $this->assertEquals($expectedData, $actualData);
 
-        $this->unsetSymfonyEnvVars();
+        $actualDataJson = json_encode($actualData, JSON_THROW_ON_ERROR);
+
+        $this->assertStringContainsStringIgnoringCase(self::APP_SECRET, $actualDataJson);
+        $this->assertStringNotContainsStringIgnoringCase(str_replace('/', '\\/', self::DATABASE_DSN), $actualDataJson);
     }
 }
